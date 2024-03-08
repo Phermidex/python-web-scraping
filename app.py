23876e81ef672d67a3185ebf655ab1c3030cac0e
Hermides Miguel Medina Morales <hermides07@gmail.com>
2024-03-08 15:12:11 -0400

    fix: issues with pylint

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 
 import contentHtml
 import file
-class App:
 
+""" App for web scraping """
+class App:
+    """ url is the url you want to scraping """    
     def __init__(self, url):
         self.url = url
         self.html = ""
-
+    """ returns the content of the url """
     def content_html(self):
         return contentHtml.content(self.url).getHtml()
 
@@ -15,11 +17,8 @@ class App:
 
 ## run script and enter name, url and press enter: python app.py
 customFileName = input("Enter file name: ")
-customUrl = input("Enter url: ")
-
-file.manager("%s.txt" % customFileName).write(App(customUrl).content_html())
-
-read_file = file.manager("%s.txt" % customFileName).read()
-
+customUrl = input("Enter url: ");
+file.manager("%s.txt" % customFileName).write(App(customUrl).content_html());
+read_file = file.manager("%s.txt" % customFileName).read();
 print("Done! our file are successfully created. Check your file is called '%s.txt' and this is your content\n\n\n%s." % (customFileName, read_file))
 
