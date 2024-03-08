ca32473bd9d4f1a24bbe481bd734a95d14f504b5
Hermides Miguel Medina Morales <hermides07@gmail.com>
2024-03-08 15:13:52 -0400

    remove unnecessary semi-colon

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ class App:
 
 ## run script and enter name, url and press enter: python app.py
 customFileName = input("Enter file name: ")
-customUrl = input("Enter url: ");
-file.manager("%s.txt" % customFileName).write(App(customUrl).content_html());
-read_file = file.manager("%s.txt" % customFileName).read();
+customUrl = input("Enter url: ")
+
+file.manager("%s.txt" % customFileName).write(App(customUrl).content_html())
+read_file = file.manager("%s.txt" % customFileName).read()
 print("Done! our file are successfully created. Check your file is called '%s.txt' and this is your content\n\n\n%s." % (customFileName, read_file))
 
